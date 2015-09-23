c97c9d41036cc4f39a802d6378a729458f334435
ellatronic <ellachiangg@gmail.com>
2015-09-22 21:38:59 -0700

    basic todo list functionality added

--- project-demos/todo-list.js
+++ project-demos/todo-list.js
@@ -1,34 +1,50 @@
-/* create a text input for adding new items to the TODO list
-When you hit enter, a new TODO item should appear on your TODO list
-The text input should be cleared
-Each TODO item should have a checkbox and a corresponding label
-
-Use CSS to make the text input bigger
-
-
-/*
-  Your Javascript here
-*/
 function toDoList() {
     var newField = document.getElementById('item');
     newField.addEventListener("keyup", addItem);
+    
+    //when unchecked, make todo item not crossed out
+    //buttons, when clicked, filter done/not done items
+    //make delete buttons for each item
+
+    //for counting unique ids
+    var count = 0;
 
     function addItem(event) {
 
+    	//if enter is pressed
+    	//get the 'list-items' ul
         if (event.keyCode === 13) {
-          var list = document.getElementById('list');
-        //create a new html element
-        //<p></p>
-          var newItem = document.createElement('p');
-        //that element should contain the value of input textbox
-        //<p>heello</p>
-          newItem.innerText = newField.value;
-        //add that new element to our list
+          var listItems = document.getElementById("list-items");
 
-          list.appendChild(newItem);
+        //create new li with unique id
+        var newLi = document.createElement("li");
+
+        //create a new checkbox
+        var newCheckbox = document.createElement("input");
+        newCheckbox.setAttribute("type", "checkbox");
+        newCheckbox.id = "item-id" + count;
+        count++;
+
+        //create a new label with value of input text
+        var newItem = document.createElement("label");
+        newItem.innerHTML = newField.value;
+
+        //add that new elements to todo list
+        listItems.appendChild(newLi);
+        newLi.appendChild(newCheckbox);
+        newLi.appendChild(newItem);
+
         //clear out text box
           newField.value = "";
         }
     }
+
+    /*var newField = document.getElementById("item");
+    newField.addEventListener("keyup", addItem);
+    //when box is checked, make todo item crossed out
+    //when item checked, log id of item to console
+    function checkItem(event) {
+    	if (event.)
+    }*/
 }
 toDoList();
