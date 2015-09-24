4dd708db0cd7537006e17160eb13320a85d11345
ellatronic <ellachiangg@gmail.com>
2015-09-23 21:24:21 -0700

    minor fixes

--- project-demos/google-book-search.js
+++ project-demos/google-book-search.js
@@ -2,18 +2,25 @@ $(document).ready(function() {
     
 
     $('#button').click(function() {
-        var input = $('#textarea').val();
+
+        //get book results
         $.get("https://www.googleapis.com/books/v1/volumes", {
             q:input,
             startIndex: 0,
             maxResults: 20},
-            function(data) {
-                //display title
-                //display author
-                //display image
-            });
+            
+        }, function(data) {
+            //display title
+            //display author
+            //display image
         });
-    });  
-});
+
+        //enter search terms
+        function search() {
+            var input = $('#textarea').val();
+        }
+    });
+});  
+
 
 //  /volumes?q=search+terms&maxResults=20
